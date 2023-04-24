42a7feab5ec84ab3b277670b66f10145a5cd3802
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-24 18:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-window.promises = [];
+const result=document.getElementById("output")
+let promise1=new Promise(function(resolve){
+	setTimeout(()=>{
+		resolve(1)
+	},Math.floor(Math.random()*5000))
+})
+let promise2=new Promise(function(resolve){
+	setTimeout(()=>{
+		resolve(2)
+	},Math.floor(Math.random()*5000))
+})
+let promise3=new Promise(function(resolve){
+	setTimeout(()=>{
+		resolve(3)
+	},Math.floor(Math.random()*5000))
+})
+let promise4=new Promise(function(resolve){
+	setTimeout(()=>{
+		resolve(4)
+	},Math.floor(Math.random()*5000))
+})
+let promise5=new Promise(function(resolve){
+	setTimeout(()=>{
+		resolve(5)
+	},Math.floor(Math.random()*5000))
+})
+window.promises = [promise2,promise2,promise3,promise4,promise5];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+promises.then((value)=>{
+	result.innerText=value;
+})
